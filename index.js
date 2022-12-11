336a93067c2085293cb2177e741d89e06a11860a
Samuel Kosasih <75837551+SamAdrn@users.noreply.github.com>
2022-12-11 15:37:10 -0500

    Initialized Files

---
+++ index.js
@@ -0,0 +1,68 @@
+// ==================== Importing Modules ====================
+
+// Include express framework to utilize network features
+const express = require("express");
+
+// Include node:path module to work with files and directory paths
+const path = require("path");
+
+// Include body-parser to parse URL encoded data
+const bodyParser = require("body-parser");
+
+// Import credentials environment file
+require("dotenv").config({ path: path.resolve(__dirname, "credentials/.env") });
+
+// ==================== Setting up Express ====================
+
+// Create an Express application
+const app = express();
+
+// Set up Express to access the templates folder
+app.set("views", path.resolve(__dirname, "templates"));
+app.set("view engine", "ejs");
+
+// Set up Express to access public folder for CSS stylesheets
+app.use(express.static("public"));
+
+// Set up Express to use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Initialize command line
+process.stdin.setEncoding("utf8");
+
+// Store port number
+const portNumber = 3000;
+
+// Start the server
+app.listen(portNumber);
+console.log(`Web server started and running at http://localhost:${portNumber}`);
+
+// ==================== Setting up MongoDB ====================
+
+// Include the MongoDB driver for Node.js
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+/* Object to store details related to our databse */
+const mongo = {
+    user: process.env.MONGO_DB_USERNAME,
+    pass: process.env.MONGO_DB_PASSWORD,
+    dbName: process.env.MONGO_DB_NAME,
+    collection: process.env.MONGO_COLLECTION,
+};
+
+// Establish a connection to our database
+const uri =
+    `mongodb+srv://${mongo.user}:${mongo.pass}` +
+    `@cluster0.ykceako.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+// ==================== Web Server Processing ====================
+
+// Show index.ejs in GET request to root page
+app.get("/", (request, response) => {
+    response.render("index");
+});
